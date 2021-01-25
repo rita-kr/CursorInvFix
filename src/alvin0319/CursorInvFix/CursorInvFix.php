56272d309543c4ca57aeb0c26bf233af9751b28f
PresentKim <debe3721@gmail.com>
2021-01-26 02:43:03 +0900

    refactor : Support API 4.0.0

--- src/alvin0319/CursorInvFix/CursorInvFix.php
+++ src/alvin0319/CursorInvFix/CursorInvFix.php
@@ -9,20 +9,15 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 
 class CursorInvFix extends PluginBase implements Listener{
+    public function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 
-	public function onEnable() : void{
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
-
-	/**
-	 * @param InventoryTransactionEvent $event
-	 *
-	 * @priority MONITOR
-	 */
-	public function onInventoryTransaction(InventoryTransactionEvent $event) : void{
-		$player = $event->getTransaction()->getSource();
-		if($event->isCancelled()){
-			$player->getCursorInventory()->sendSlot(0, $player);
-		}
-	}
+    /** @priority MONITOR */
+    public function onInventoryTransaction(InventoryTransactionEvent $event) : void{
+        $player = $event->getTransaction()->getSource();
+        if($event->isCancelled()){
+            $player->getNetworkSession()->getInvManager()->syncContents($player->getCursorInventory());
+        }
+    }
 }
